2865c88835da124fc805258d23dc82bcc82fc7b3
Severiano Sisneros <seve.sisneros@protonmail.com>
2024-05-23 01:12:11 -0700

    feat: Updated guest program to prove using ticks
    BREAKING CHANGE:

--- guest/src/lib.rs
+++ guest/src/lib.rs
@@ -2,41 +2,30 @@
 #![no_main]
 
 extern crate alloc;
-use alloc::vec::Vec;
-
-use alloy_primitives::U256;
-use swap::SwapValues;
-
-
-
-#[jolt::provable(max_input_size = 10000, stack_size = 10000, memory_size = 10000000)]
-fn volatility(swap_values: Vec<SwapValues>) -> U256 {
-    // let n = U256::from(swap_values.len()); // Convert swap_values.len() to U256
-    let mut sum_ui = U256::from(0); // Convert 0 to U256
-    // let mut sum_ui2 = U256::from(0); // Convert 0 to U256
-    for i in 1..swap_values.len() {
-        let u_i = swap_values[i].calculate_price() / swap_values[i - 1].calculate_price();
-        // let u_i = swap_values[i].calculate_price() ;
-        sum_ui += u_i;
-        // sum_ui2 += u_i*u_i;
-    }
-
-    // let s2 =(sum_ui2 / (n - U256::from(1))) - (sum_ui*sum_ui / (n*(n - U256::from(1)))); 
-    sum_ui
-    // s2
-    // swap_values[0].calculate_price()
-}
+use alloc::vec::Vec as Avec;
 
 #[jolt::provable]
-fn fib(n: u32) -> u128 {
-    let mut a: u128 = 0;
-    let mut b: u128 = 1;
-    let mut sum: u128;
-    for _ in 1..n {
-        sum = a + b;
-        a = b;
-        b = sum;
-    }
+fn volatility( 
+    s: f64,
+    ticks: Avec<u32>,
+    inv_n: f64,
+    inv_n1: f64 
+) -> bool {
 
-    b
+
+    let mut sum_ui = 0f64;
+    let mut sum_ui2 = 0f64;
+    let n = ticks.len();
+    for i in 1..n {
+        let ui = ticks[i] as f64 - ticks[i - 1] as f64;
+        sum_ui += ui;
+        sum_ui2 += ui * ui; 
+    }
+    
+    let s2 = inv_n1 as f64 * (sum_ui2 - sum_ui * sum_ui * inv_n as f64);
+    let s_check = s2 == s * s;
+    let n_check = n as f64 * inv_n == 1.0;
+    let n1_check = (n - 1) as f64 * inv_n1 == 1.0;
+    return s_check & n_check & n1_check
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,30 @@
-use std::{error::Error, io};
-use csv;
-use alloy_primitives::{address, Address, BlockNumber, TxHash, TxIndex, U256};
-use swap::SwapValues;
 
-// Specify a struct named Event with the following fields: amount0in,amount0out,amount1in,amount1out,contract_address,evt_block_number,evt_block_time,evt_index,evt_tx_hash,sender,to
-#[derive(Debug, serde::Deserialize)]
-struct Event {
-    amount0in: U256,
-    amount0out: U256,
-    amount1in: U256,
-    amount1out: U256,
-    contract_address: Address,
-    evt_block_number: BlockNumber,
-    evt_block_time: String,
-    evt_index: TxIndex  ,
-    evt_tx_hash: TxHash,
-    sender: Address,
-    to: Address,
-}
-
-
-fn read_events() -> Result<Vec<Event>, Box<dyn Error>> {
-    println!("Reading events from file");
-    let mut rdr = csv::Reader::from_path("uniswap-v2-events.csv")?;
-    // let mut rdr = csv::Reader::from_reader(io::stdin());
-    let mut event_vec = Vec::new();
-    for result in rdr.deserialize() {
-        // Notice that we need to provide a type hint for automatic
-        // deserialization.
-        let event: Event = result?;
-        event_vec.push(event);
-    }
-
-    Ok(event_vec)
-}
-
+extern crate alloc;
+use alloc::vec::Vec as Avec;
 
 pub fn main() {
-    let usdt_weth_pool: Address = address!("0d4a11d5EEaaC28EC3F61d100daF4d40471f1852");
-    let mut usdt_weth_swaps = Vec::new();
-    let events = read_events().unwrap();
-    println!("Num events = {:?}", events.len());
-    for event in events.iter() {
-        if event.contract_address == usdt_weth_pool {
-            let swap = SwapValues::new(event.amount0in, event.amount0out, event.amount1in, event.amount1out);
-            usdt_weth_swaps.push(swap);
-        }
+    // Create a vector of u32 tick values for test
+    let mut ticks = Avec::new();
+    for i in 0..16 {
+        ticks.push(i % 8);
     }
 
-    let (prove_volatility, verify_volatility) = guest::build_volatility();
-    let test_vec = &usdt_weth_swaps[0..2];
-    println!("length test_vec = {:?}", test_vec.len());
-    println!("test_vec = {:?}", test_vec);
-    let (output, proof) = prove_volatility(test_vec.to_vec());
-    let is_valid = verify_volatility(proof);
+    let inv_n = 1.0/ticks.len() as f64;
+    let inv_n1 = 1.0/(ticks.len() - 1) as f64;
 
-    println!("output: {}", output);
-    println!("valid: {}", is_valid);
+    let mut sum_ui = 0f64;
+    let mut sum_ui2 = 0f64;
+    for i in 1..ticks.len() {
+        let ui = ticks[i] as f64 - ticks[i - 1] as f64;
+        sum_ui += ui;
+        sum_ui2 += ui * ui; 
+    }
+    let s2 = inv_n1 as f64 * (sum_ui2 as f64 - sum_ui as f64 * sum_ui as f64 * inv_n as f64);
+    let s = s2.sqrt();
+    // s2
+    let (prove_volatility, verify_volatility) = guest::build_volatility();
+    let (output, proof) = prove_volatility(s, ticks, inv_n, inv_n1);
+    let is_valid = verify_volatility(proof);
+    assert!(is_valid);
+    assert_eq!(output, true);
 }
